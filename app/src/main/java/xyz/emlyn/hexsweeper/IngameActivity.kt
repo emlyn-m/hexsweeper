3df171d5662a63134751c61468b4446335b302f7
Emlyn Matheson <emlynmatheson@gmail.com>
2022-03-17 20:44:19 +1300

    Initial Commit

---
+++ app/src/main/java/xyz/emlyn/hexsweeper/IngameActivity.kt
@@ -0,0 +1,4 @@
+package xyz.emlyn.hexsweeper
+
+class IngameActivity {
+}
---
+++ app/src/main/java/xyz/emlyn/hexsweeper/MenuActivity.kt
@@ -0,0 +1,11 @@
+package xyz.emlyn.hexsweeper
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
